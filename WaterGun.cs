e357748e525a3fa0affd354342eef2acb2beabc6
cherrycookie27 <158618742+cherrycookie27@users.noreply.github.com>
2025-05-05 12:06:23 +0300

    Update WaterGun.cs

--- WaterGun.cs
+++ WaterGun.cs
@@ -1,3 +1,5 @@
+//The watergun code. This was pretty simple with only spawning the bullet when needed.
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,6 +15,7 @@ public class WaterGun : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
+            //Bullet gets spawned
             Instantiate(dropletPrefab, shootingPoint.position, transform.rotation);
             watersound.Play();
         }
